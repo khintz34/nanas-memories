fc60236cf0564c46c9256f97d0ba086f5ff46c72
Kevin Hintz <khintz34@gmail.com>
2023-08-03 10:22:33 -0500

    tree Image fix

--- src/app/FamilyTree/page.tsx
+++ src/app/FamilyTree/page.tsx
@@ -1,11 +1,33 @@
+"use client";
+
 import styles from "./page.module.scss";
 import Image from "next/image";
 import tree from "../../assets/images/familyTree.png";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 700) {
+      setMobile(false);
+    } else {
+      setMobile(true);
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
-      <Image height={500} width={800} src={tree} alt="Nana's Family Tree" />
+      {mobile ? (
+        <Image height={250} width={400} src={tree} alt="Nana's Family Tree" />
+      ) : (
+        <Image
+          height={500}
+          width={750}
+          src={tree}
+          alt="Nana's Family Tree Large"
+        />
+      )}
     </main>
   );
 }
