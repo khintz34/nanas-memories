e65668a40ff18bd1ea478a8906b0e53659355d5a
Kevin Hintz <khintz34@gmail.com>
2023-08-03 19:47:57 -0500

    window

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "firebasestorage.googleapis.com",
-  //       port: "",
-  //       pathname: "/v0/b/nana-s-memories.appspot.com/o/",
-  //     },
-  //   ],
-  // },
 };
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,35 +75,7 @@ export default function Home() {
         title="Orchestra Concert"
         year=""
         tags={["Nana"]}
-      />
-      <Memory
-        pic={christening}
-        description="Jim's christening party. From the left: great aunt Ella, grandma Esther Zoschke,  uncle David Z, grandma Bertha Hintz and grandpa Paul H, Sr."
-        title="Jim's Christening"
-        year=""
-        tags={["Ella", "Esther", "David", "Bertha", "Paul Sr."]}
-      />
-      <Memory
-        pic={walter}
-        description="Nana's cousin Walter Z with grandpa Wilhelm (or William) Z at grandma and grandpa's 50th anniversary. Walter gave Nana a blood transfusion when her appendix burst, helping to save her life. He was a bus driver, but sadly had a heart attack and died fairly young at 62."
-        title="Walter Zoschke and Grandpa Wilhelm"
-        year=""
-        tags={["Walter", "Wilhelm"]}
-      />
-      <Memory
-        pic={santa}
-        description="Jim in '57. Do you recognize the visitor in the living room?"
-        title="The Infamous Santa"
-        year="1957"
-        tags={["Jim"]}
-      />
-      <Memory
-        pic={farm}
-        description="The farm which was a Christmas gift that year. It was so long ago that the window Bill is leaning on hadn't been removed yet to make space for the family room."
-        title="The Farm"
-        year=""
-        tags={["Bill"]}
-      /> */}
+        /> */}
 
       {memoryList?.map((val, index) => {
         console.log(val.image);
