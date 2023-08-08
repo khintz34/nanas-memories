b18e173a7ab2dba2fe8965d7c035cb878093d4b9
Kevin Hintz <khintz34@gmail.com>
2023-08-08 16:26:45 -0500

    input basic

--- src/app/AddMemory/page.tsx
+++ src/app/AddMemory/page.tsx
@@ -4,10 +4,27 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h2>Add a Memory</h2>
-      <form action="">
-        <label htmlFor="memName">Memory Name</label>
-        <input type="text" id="memName" />
+      <h2 className={styles.h2}>Add a Memory</h2>
+      <form action="" className={styles.form}>
+        <div className={styles.inputContainer}>
+          <label htmlFor="memName">Memory Name</label>
+          <input type="text" id="memName" />
+        </div>
+
+        <div className={styles.inputContainer}>
+          <label htmlFor="memTags">
+            Who is in this memory? (Separate with commas)
+          </label>
+          <input type="text" id="memTags" />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="memYear">Year (optional)</label>
+          <input type="text" id="memYear" />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="memDesc">Description</label>
+          <textarea id="memDesc" className={styles.textArea} />
+        </div>
       </form>
     </main>
   );
